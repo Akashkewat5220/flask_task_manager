99b0bc2b88333dcb087c8187ea2e34383dbb5cb5
Akash kewat <kewatakash42@gmail.com>
2025-06-01 00:12:25 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,3 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_jwt_extended import JWTManager
-
-db = SQLAlchemy()
-migrate = Migrate()
-jwt = JWTManager()
-
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
@@ -16,10 +7,11 @@ def create_app():
     migrate.init_app(app, db)
     jwt.init_app(app)
 
-    from app.routes import task_bp
+    from app.routes import task_bp, main
     from app.auth import auth_bp
 
     app.register_blueprint(task_bp)
     app.register_blueprint(auth_bp)
+    app.register_blueprint(main)
 
     return app
