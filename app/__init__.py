a22b9813a634e1fc2e79ca1299ef6b6e9c250191
Akash kewat <kewatakash42@gmail.com>
2025-06-01 00:20:28 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+
+db = SQLAlchemy()       # Initialize db here
+migrate = Migrate()
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
-    app.config['JWT_SECRET_KEY'] = 'S3cureJWTKey_2025!FlaskAuth'
+    app.config['JWT_SECRET_KEY'] = 'aS3cureR@nd0mStr1ng!'
 
-    db.init_app(app)
+    db.init_app(app)         # Use db after initializing it
     migrate.init_app(app, db)
     jwt.init_app(app)
 
